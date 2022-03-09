4a7cbfb24b11e4076e9db7ba8fc0851480c8e7da
Cesarq19 <90804290+Cesarq19@users.noreply.github.com>
2022-03-09 15:16:39 -0500

    Avance dia 1

---
+++ juego.py
@@ -0,0 +1,7 @@
+class juego:
+    """Clase juego
+    Aqui transcurren las interacciones entre las fichas de juego"""
+    def __init__(self,id):
+        self.id = id
+
+        
---
+++ jugador.py
@@ -0,0 +1,5 @@
+class jugador:
+    """Clase jugador"""
+    def __init__(self,name,posicion):
+        self.name = name
+        self.posicion = posicion
